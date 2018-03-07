90040c142ef3fbeda918d7f8bf5dc7d7294b6f3a
Peter Karlsson <peter.karlsson90@icloud.com>
2018-03-07 13:17:54 +0100

    Initial Commit

---
+++ Improve Your LifestyleTests/Improve_Your_LifestyleTests.swift
@@ -0,0 +1,36 @@
+//
+//  Improve_Your_LifestyleTests.swift
+//  Improve Your LifestyleTests
+//
+//  Created by Peter Karlsson on 2018-03-07.
+//  Copyright © 2018 Peter Karlsson. All rights reserved.
+//
+
+import XCTest
+@testable import Improve_Your_Lifestyle
+
+class Improve_Your_LifestyleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
